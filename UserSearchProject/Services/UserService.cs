6f1f28b919549fe534cf36c60ecca0b273f444e2
bradonf333 <bradonf123@gmail.com>
2023-02-12 10:47:51 -0700

    Service now returns user if its a match, regardless of case

--- UserSearchProject/Services/UserService.cs
+++ UserSearchProject/Services/UserService.cs
@@ -12,21 +12,25 @@ namespace UserSearchProject.Services
             _dbRepository = dbRepository;
         }
 
-        public IEnumerable<User> GetUsers(string firstName = null, string lastName = null, string email = null)
+        public IEnumerable<User> GetUsers(string? firstName = null, string? lastName = null, string? email = null)
         {
+            firstName = firstName?.ToLower(System.Globalization.CultureInfo.CurrentCulture);
+            lastName = lastName?.ToLower(System.Globalization.CultureInfo.CurrentCulture);
+            email = email?.ToLower(System.Globalization.CultureInfo.CurrentCulture);
+
             IEnumerable<User> users;
             if(!string.IsNullOrWhiteSpace(firstName) && !string.IsNullOrWhiteSpace(lastName))
             {
                 users = _dbRepository.GetUsers()
-                .Where(u => u.FirstName == firstName)
-                .Where(u => u.LastName == lastName);
+                .Where(u => u.FirstName?.ToLower(System.Globalization.CultureInfo.CurrentCulture) == firstName)
+                .Where(u => u.LastName?.ToLower(System.Globalization.CultureInfo.CurrentCulture) == lastName);
             }
             else
             {
                 users = _dbRepository.GetUsers()
-               .Where(u => u.FirstName == firstName
-               || u.LastName == lastName
-               || u.Email == email);
+               .Where(u => u.FirstName?.ToLower(System.Globalization.CultureInfo.CurrentCulture) == firstName
+               || u.LastName?.ToLower(System.Globalization.CultureInfo.CurrentCulture) == lastName
+               || u.Email?.ToLower(System.Globalization.CultureInfo.CurrentCulture) == email);
             }
 
             return users;
