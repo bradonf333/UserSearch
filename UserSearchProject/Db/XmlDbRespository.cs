0e3fd356695f62c8ddbb6f7c995df6e76011ccbf
bradonf333 <bradonf123@gmail.com>
2023-02-11 18:20:04 -0700

    Hooked up xml file to db and did some test refactor

--- UserSearchProject/Db/XmlDbRespository.cs
+++ UserSearchProject/Db/XmlDbRespository.cs
@@ -1,4 +1,6 @@
-﻿using UserSearchProject.Data;
+﻿using Microsoft.AspNetCore.Http;
+using System.Xml.Linq;
+using UserSearchProject.Data;
 
 namespace UserSearchProject.Db
 {
@@ -6,7 +8,28 @@ namespace UserSearchProject.Db
     {
         public IEnumerable<User> GetUsers()
         {
-            return Enumerable.Empty<User>();
+            var filename = "Users.xml";
+            var currentDirectory = Directory.GetCurrentDirectory();
+            var usersFilePath = Path.Combine(currentDirectory, filename);
+            var mainElement = XElement.Load(usersFilePath);
+
+            var userElements = mainElement.Elements().ToList();
+
+            List<User> users = userElements.Select(u => new User()
+            {
+                Id = (int)u.Attribute("id"),
+                ClientId = (int)u.Element("ClientId"),
+                FirstName = (string)u.Element("FirstName"),
+                LastName = (string)u.Element("LastName"),
+                Email = (string)u.Element("Email"),
+                IsBusinessContact = (bool)u.Element("IsBusinessContact"),
+                IsAccountingContact = (bool)u.Element("IsAccountingContact"),
+                IsTechnicalContact = (bool)u.Element("IsTechnicalContact"),
+                HasApiAccess = (bool)u.Element("HasApiAccess"),
+            }).ToList();
+
+
+            return users;
         }
     }
 }
